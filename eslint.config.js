43e6bdd89f8082a5d25a1bae4455bd5624ad5610
kokoryh <kokoryh@qq.com>
2025-04-09 07:57:20 +0800

    tsconfig

---
+++ eslint.config.js
@@ -0,0 +1,5 @@
+import eslint from '@eslint/js';
+import { globalIgnores } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, globalIgnores(['dist']));
