9c1c64fa30797cd8804ae6a3f0013e13fcfe7450
kokoryh <kokoryh255@gmail.com>
2025-07-05 10:06:20 +0800

    refactor(12306): build script with new env

---
+++ src/script/12306/handler.ts
@@ -0,0 +1,45 @@
+import Client from '@core/client';
+import { AbstractHandler, JsonHandler } from '@core/handler';
+import { AdListReq } from '@entity/12306';
+
+export const $ = Client.getInstance('12306');
+
+export function createHandler(url: string): AbstractHandler | null {
+    if (url.endsWith('/getAdList')) {
+        return new AdListHandler($.request.body as string);
+    } else if (url.endsWith('/mgw.htm')) {
+        return new MgwHtmHandler();
+    }
+    return null;
+}
+
+export class AdListHandler extends JsonHandler<AdListReq> {
+    done(): void {
+        $.done({ response: { body: this.getResponseBody() } });
+    }
+
+    private getResponseBody(): string {
+        const { placementNo } = this.message;
+        switch (placementNo) {
+            case '0007':
+                return '{"materialsList":[{"billMaterialsId":"1","filePath":"#","creativeType":1}],"advertParam":{"skipTime":1}}';
+            case 'G0054':
+                return '{"code":"00","materialsList":[{}]}';
+            default:
+                return '{"code":"00","message":"0"}';
+        }
+    }
+}
+
+export class MgwHtmHandler extends AbstractHandler {
+    private filterList = ['com.cars.otsmobile.newHomePageBussData'];
+
+    done(): void {
+        this.isAd() ? $.abort() : $.exit();
+    }
+
+    private isAd(): boolean {
+        const headers = $.request.headers;
+        return this.filterList.includes(headers['operation-type']);
+    }
+}
