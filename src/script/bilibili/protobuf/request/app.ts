b3bd5a230989bd1b49cf58d6ba27c6846823bebf
kokoryh <kokoryh255@gmail.com>
2025-11-28 19:14:50 +0800

    fix(bilibili): resolve log issue

--- src/script/bilibili/protobuf/request/app.ts
+++ src/script/bilibili/protobuf/request/app.ts
@@ -1,9 +1,10 @@
 import { Application } from '@core/application';
 import { doneFakeResponse } from '@core/middleware';
+import { withArgument } from '../middleware';
 import router from './router';
 
 const app = new Application();
 
-app.use(doneFakeResponse).use(router.routes());
+app.use(doneFakeResponse).use(withArgument).use(router.routes());
 
 export default app;
--- src/script/bilibili/protobuf/request/router.ts
+++ src/script/bilibili/protobuf/request/router.ts
@@ -7,14 +7,13 @@ import {
     handleViewReply,
     handleMainListReply,
 } from '../handler';
-import { withArgument } from '../middleware';
 
 const router = new Router({
     matchPath: (layer, ctx) => ctx.request.url.endsWith(layer.path as string),
 });
 
-router.add('v1.DM/DmSegMobile', withArgument, handleDmSegMobileReq, parseGrpcResponse, handleDmSegMobileReply);
+router.add('v1.DM/DmSegMobile', handleDmSegMobileReq, parseGrpcResponse, handleDmSegMobileReply);
 router.add('viewunite.v1.View/View', handleRequest, parseGrpcResponse, handleViewReply);
-router.add('v1.Reply/MainList', withArgument, handleRequest, parseGrpcResponse, handleMainListReply);
+router.add('v1.Reply/MainList', handleRequest, parseGrpcResponse, handleMainListReply);
 
 export default router;
